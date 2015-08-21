6c3598b0bc5fc3ea2fa396edab5ddb44547081cd
Franziska Sauerwein <franzi@codurance.com>
2015-08-21 10:23:52 +0100

    tag end-to-end tests in cucumber

--- src/test/scala/gr8craft/features/CucumberFeatures.scala
+++ src/test/scala/gr8craft/features/CucumberFeatures.scala
@@ -1,8 +1,10 @@
 package gr8craft.features
 
+import cucumber.api.CucumberOptions
 import cucumber.api.junit.Cucumber
 import org.junit.runner.RunWith
 
+@CucumberOptions(tags = Array("@end-to-end"))
 @RunWith(classOf[Cucumber])
 class CucumberFeatures {
 }
