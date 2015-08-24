cc9436aabfc2c0bb7fe3ff63c7110a4b49f42aac
Franziska Sauerwein <franzi@codurance.com>
2015-08-24 11:52:25 +0100

    configure Twitter with environment variables that do not contain dots to allow travis to read them

--- src/main/scala/gr8craft/ApplicationRunner.scala
+++ src/main/scala/gr8craft/ApplicationRunner.scala
@@ -5,7 +5,9 @@ import java.util.concurrent.TimeUnit.HOURS
 import gr8craft.article.{Article, InMemoryShelf}
 import gr8craft.scheduling.{ScheduledExecutor, Scheduler}
 import gr8craft.twitter.{TweetRunner, TwitterApiService}
+import twitter4j.TwitterFactory
 import twitter4j.TwitterFactory.getSingleton
+import twitter4j.conf.ConfigurationBuilder
 
 class ApplicationRunner(scheduler: Scheduler) {
   def startTwitterBot() {
@@ -28,7 +30,19 @@ object ApplicationRunner {
 
   def assembleApplication: ApplicationRunner = {
     val articles: List[Article] = List(new Article("dummy topic", "dummy URL"))
-    val tweetRunner: TweetRunner = new TweetRunner(new TwitterApiService(getSingleton), new InMemoryShelf(articles))
+    val tweetRunner: TweetRunner = new TweetRunner(createTwitterApiService, new InMemoryShelf(articles))
     new ApplicationRunner(new ScheduledExecutor(HOURS, tweetRunner))
+  }
+
+  def createTwitterApiService: TwitterApiService = {
+    val configuration = new ConfigurationBuilder()
+      .setDebugEnabled(true)
+      .setOAuthConsumerKey(sys.env("twitter4jconsumerKey"))
+      .setOAuthConsumerSecret(sys.env("twitter4jconsumerSecret"))
+      .setOAuthAccessToken(sys.env("twitter4jaccessToken"))
+      .setOAuthAccessTokenSecret(sys.env("twitter4jaccessTokenSecret"))
+      .build()
+
+    new TwitterApiService(new TwitterFactory(configuration).getInstance())
   }
 }
