71b550b1251f09fe97d586052949a0457a9b0303
Franziska Sauerwein <franzi@codurance.com>
2015-09-15 13:50:26 +0100

    provide toString method for articles

--- src/main/scala/gr8craft/article/Article.scala
+++ src/main/scala/gr8craft/article/Article.scala
@@ -4,4 +4,6 @@ class Article(_topic: String, _location: String) {
   def topic = _topic
 
   def location = _location
+
+  override def toString: String = "topic: %s location: %s".format(topic, location)
 }
