59feed68f8d4f93a7377d6cc3f688de97a288c9e
Franziska Sauerwein <franzi@codurance.com>
2015-09-16 16:05:53 +0100

    shut down Actor System after test

--- src/test/scala/gr8craft/twitter/TweetRunnerShould.scala
+++ src/test/scala/gr8craft/twitter/TweetRunnerShould.scala
@@ -1,16 +1,17 @@
 package gr8craft.twitter
 
 import akka.actor.{ActorSystem, Props}
+import akka.testkit.TestKit._
 import akka.testkit.{TestProbe, TestKit}
 import gr8craft.inspiration.{Inspiration, Shelf}
 import gr8craft.messages.{Tweet, AddInspiration, Trigger}
 import org.junit.runner.RunWith
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.FunSuiteLike
+import org.scalatest.{BeforeAndAfterAll, FunSuiteLike}
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class TweetRunnerShould extends TestKit(ActorSystem("TweetRunnerShould")) with FunSuiteLike with MockFactory {
+class TweetRunnerShould extends TestKit(ActorSystem("TweetRunnerShould")) with FunSuiteLike with MockFactory with BeforeAndAfterAll {
   val topic = "topic"
   val location = "location"
   val inspiration = new Inspiration(topic, location)
@@ -19,7 +20,11 @@ class TweetRunnerShould extends TestKit(ActorSystem("TweetRunnerShould")) with F
   val tweeter = TestProbe()
 
   val tweetRunner = system.actorOf(Props(new TweetRunner(tweeter.ref, shelf)))
-  
+
+  override def afterAll() {
+    shutdownActorSystem(system)
+  }
+
   test("tweet the first inspiration from the shelf") {
     (shelf.next _).expects().returns(inspiration)
 
@@ -36,7 +41,6 @@ class TweetRunnerShould extends TestKit(ActorSystem("TweetRunnerShould")) with F
     tweetRunner ! AddInspiration(inspiration)
 
     tweetRunner ! Trigger
-
     tweeter.expectMsg(Tweet("Your hourly recommended inspiration about " + topic + ": " + location))
   }
 }
