77ad093250c4315619aaf476c9ca2bc8313bdab7
Franziska Sauerwein <franzi@codurance.com>
2015-10-06 17:15:08 +0100

    restore link to moved step definitions

--- src/test/scala/com/codurance/gr8craft/featuresmocked/MockedCucumberFeatures.scala
+++ src/test/scala/com/codurance/gr8craft/featuresmocked/MockedCucumberFeatures.scala
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber
 import org.junit.runner.RunWith
 
 @RunWith(classOf[Cucumber])
-@CucumberOptions(features = Array("src/test/resources"), glue = Array("gr8craft.featuresmocked"), tags = Array("~@work-in-progress"))
+@CucumberOptions(features = Array("src/test/resources"), glue = Array("com.codurance.gr8craft.featuresmocked"), tags = Array("~@work-in-progress"))
 class MockedCucumberFeatures {
 }
