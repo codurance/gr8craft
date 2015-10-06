2deaf0500d2d526a9a1396aae8c5517e5343857a
Franziska Sauerwein <franzi@codurance.com>
2015-10-06 18:48:23 +0100

    allow for spaces in submissions

--- src/main/scala/com/codurance/gr8craft/model/inspiration/Suggestion.scala
+++ src/main/scala/com/codurance/gr8craft/model/inspiration/Suggestion.scala
@@ -1,12 +1,12 @@
 package com.codurance.gr8craft.model.inspiration
 
 class Suggestion(toParse: String) {
-  private val PATTERN_FOR_SUGGESTIONS = """inspiration: (\S+) \| location: (\S+) \| contributor: (\S+)""".r
+  val PatternForNewInspirations = """inspiration: ([\s\S]+) \| location: ([\s\S]+) \| contributor: ([\s\S]+)""".r
 
   def parse: Option[Inspiration] =
     toParse match {
-      case PATTERN_FOR_SUGGESTIONS(topic, location, contributor) =>
-        Some(new Inspiration(topic, location, Some(contributor)))
-      case _ => None
+      case PatternForNewInspirations(topic, location, contributor) =>
+        Option.apply(new Inspiration(topic, location, Option.apply(contributor)))
+      case _ => Option.empty
     }
 }
