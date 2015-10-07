6113a61f8bf0a8dfc7240e30aae3e3ec4343692c
Franziska Sauerwein <franzi@codurance.com>
2015-10-07 19:20:12 +0100

    add researcher to look for direct messages

---
+++ src/main/scala/com/codurance/gr8craft/research/Researcher.scala
@@ -0,0 +1,26 @@
+package com.codurance.gr8craft.research
+
+import akka.actor.{Actor, ActorRef}
+import com.codurance.gr8craft.messages._
+import com.codurance.gr8craft.model.inspiration.Inspiration
+import com.codurance.gr8craft.model.publishing.{DirectMessage, DirectMessageId, Tweet, TwitterService}
+
+class Researcher(twitterService: TwitterService) extends Actor {
+  private val APPROVED_MODERATOR = "gr8craftmod"
+
+  override def receive: Receive = {
+    case FetchDirectMessages(lastFetched) =>
+      fetchDirectMessages(lastFetched)
+  }
+
+  private def fetchDirectMessages(lastFetched: Option[DirectMessageId]): Unit = {
+    val actorToInform = sender()
+    twitterService.fetchDirectMessagesAfter(lastFetched, messages => addInspirations(messages, actorToInform))
+  }
+
+  def addInspirations(messages: List[DirectMessage], actorToInform: ActorRef): Unit = {
+    messages
+      .filter(message => message.sender == APPROVED_MODERATOR)
+      .foreach(message => actorToInform ! AddDirectMessage(message))
+  }
+}
