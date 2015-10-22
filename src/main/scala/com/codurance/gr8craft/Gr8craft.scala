4ee613d0783d4909bf2ca3de7b89703ebc331da9
Franziska Sauerwein <franzi@codurance.com>
2015-10-22 22:05:09 +0200

    improve clarity of application assembly

--- src/main/scala/com/codurance/gr8craft/Gr8craft.scala
+++ src/main/scala/com/codurance/gr8craft/Gr8craft.scala
@@ -23,23 +23,18 @@ object Gr8craft {
     createApplication().startTwitterBot()
   }
 
-  def createApplication(system: ActorSystem = ActorSystem("Gr8craftSystem"), tweetSender: TweetSender = new TweetSenderViaTwitter(createTwitter()), directMessageFetcher: DirectMessageFetcher = new DirectMessageFetcherViaTwitter(createTwitter()), initialInspirations: Set[Inspiration] = Set.empty, tweetInterval: Duration = 1.hour): Gr8craft = {
-
-    def createSupervisor(system: ActorSystem, twitterService: TweetSender, tweetInterval: Duration, archivist: ActorRef): ActorRef = {
-      system.actorOf(Props(new Supervisor(tweetInterval, List(createEditor(archivist), createJournalist(archivist)))))
-    }
-
-    def createJournalist(archivist: ActorRef): ActorRef = {
-      val researcher = system.actorOf(Props(new Researcher(directMessageFetcher)))
-      system.actorOf(Props(new Journalist(researcher, archivist)))
-    }
-
-    def createEditor(archivist: ActorRef): ActorRef = {
-      val publisher = system.actorOf(Props(new Publisher(tweetSender)))
-      system.actorOf(Props(new Editor(publisher, archivist)))
-    }
-
+  def createApplication(
+                         system: ActorSystem = ActorSystem("Gr8craftSystem"),
+                         tweetSender: TweetSender = new TweetSenderViaTwitter(createTwitter()),
+                         directMessageFetcher: DirectMessageFetcher = new DirectMessageFetcherViaTwitter(createTwitter()),
+                         initialInspirations: Set[Inspiration] = Set.empty,
+                         tweetInterval: Duration = 1.hour): Gr8craft = {
     val archivist = system.actorOf(Props(new Archivist(new Shelf(initialInspirations))))
-    new Gr8craft(createSupervisor(system, tweetSender, tweetInterval, archivist))
+    val researcher = system.actorOf(Props(new Researcher(directMessageFetcher)))
+    val editor = system.actorOf(Props(new Journalist(researcher, archivist)))
+    val publisher = system.actorOf(Props(new Publisher(tweetSender)))
+    val journalist = system.actorOf(Props(new Editor(publisher, archivist)))
+    val supervisor = system.actorOf(Props(new Supervisor(tweetInterval, List(editor, journalist))))
+    new Gr8craft(supervisor)
   }
 }
